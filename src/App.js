46910dd326c645fef902088369c4810d54a3779f
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-12 23:47:35 -0300

    page news with mapping each post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ola</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/New.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import api from '../services/api';
+
+export default class New extends Component {
+  
+
+  render() {
+    return (
+      <div>
+        <h1>New</h1>
+      </div>
+    )
+  }
+}
